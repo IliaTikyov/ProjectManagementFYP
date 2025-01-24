23f9cd237044583c7c33deaee385eb74e0580989
IliaTikyov <iliatikiov123@gmail.com>
2025-01-24 11:02:38 +0000

    Adding Datepicker

--- src/components/CardItems.jsx
+++ src/components/CardItems.jsx
@@ -1,6 +1,7 @@
 import { useDraggable } from "@dnd-kit/core";
+import { date } from "yup";
 
-const priorityColors = {
+const priorityOptions = {
   low: "text-white bg-green-500",
   medium: "text-white bg-yellow-500",
   high: "text-white bg-red-500",
@@ -34,12 +35,31 @@ const CardItems = ({ card, columnId }) => {
       {card.priority && (
         <span
           className={`inline-block mx-auto px-5 py-1 mt-2 text-xs font-semibold rounded-md ${
-            priorityColors[card.priority]
+            priorityOptions[card.priority]
           }`}
         >
           {card.priority.toUpperCase()}
         </span>
       )}
+      <div>
+        {new Date(card.dueDate) >= new Date() ? (
+          <p className="text-md text-blue-500 mt-2">
+            Due: {new Date(card.dueDate).toLocaleDateString()} (in
+            {Math.ceil(
+              (new Date(card.dueDate) - new Date()) / (1000 * 60 * 60 * 24)
+            )}{" "}
+            days)
+          </p>
+        ) : (
+          <p className="text-md text-red-500 mt-2">
+            Task is overdue by
+            {Math.ceil(
+              (new Date() - new Date(card.dueDate)) / (1000 * 60 * 60 * 24)
+            )}{" "}
+            days
+          </p>
+        )}
+      </div>
     </div>
   );
 };
