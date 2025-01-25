d7e9b16addf575d2514f23da60d908fe3a4ca337
IliaTikyov <iliatikiov123@gmail.com>
2025-01-25 00:42:25 +0000

    Updating the Fetch Method

--- src/appwriteConfig.jsx
+++ src/appwriteConfig.jsx
@@ -7,26 +7,6 @@ const client = new Client().setEndpoint(API_ENDPOINT).setProject(PROJECT_ID);
 
 export const account = new Account(client);
 export const database = new Databases(client);
+export const uniqueID = ID;
 
 export default client;
-
-// Function to create a task
-export const createTask = async (Title, Description, Priority, dueDate) => {
-  try {
-    await database.createDocument(
-      "67714f2e0006d28825f7",
-      "67714f5100032d069052",
-      ID.unique(),
-      {
-        Title,
-        Description,
-        Priority,
-        dueDate,
-      }
-    );
-    alert("Task Created Successfully");
-  } catch (error) {
-    console.error("DB Error >>", error);
-    alert("Encountered an Error");
-  }
-};
