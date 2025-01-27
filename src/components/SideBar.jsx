bcfc84ab34bbdfea30280720ba3418729f0e1569
IliaTikyov <iliatikiov123@gmail.com>
2025-01-27 22:24:08 +0000

    Updating The UI

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,23 +1,31 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import { useAuth } from "../utils/AuthContext";
+import { FaTasks } from "react-icons/fa";
+import { BsPeopleFill } from "react-icons/bs";
 
 import { CiLogout } from "react-icons/ci";
 
 const SideBar = ({ children }) => {
-  const { logoutUser } = useAuth();
+  const { logoutUser, user } = useAuth();
 
   return (
     <div className="min-h-screen flex">
       <div className="w-64 bg-blue-500 text-white flex flex-col p-6">
-        <Link to="/" className="mb-4 text-lg font-semibold hover:text-gray-200">
-          Task Board
+        <div className="mb-6 font-semibold flex justify-center">
+          Welcome {user ? user.name || user.email : "Guest"}
+        </div>
+        <Link
+          to="/"
+          className="mb-4 text-lg text-blue-500 font-semibold hover:text-blue-400 hover:bg-gray-200 border rounded-lg flex items-center justify-center bg-white p-2"
+        >
+          Task Board <FaTasks className="ml-2 mt-0.5" />
         </Link>
         <Link
           to="/members"
-          className="mb-4 text-lg font-semibold hover:text-gray-200"
+          className="mb-4 text-lg text-blue-500 font-semibold hover:text-blue-400 hover:bg-gray-200 border rounded-lg flex items-center justify-center bg-white p-2"
         >
-          Members
+          Members <BsPeopleFill className="ml-2 mt-0.5" />
         </Link>
         <button
           onClick={logoutUser}
